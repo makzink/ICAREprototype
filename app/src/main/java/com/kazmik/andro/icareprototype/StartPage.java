d8eadced707c20d33898f36cb3618bf31e42d673
Mohammed Akzin K <makzink@gmail.com>
2015-01-27 11:04:43 +0530

    initial commit
    done accel and speech recog

---
+++ app/src/main/java/com/kazmik/andro/icareprototype/StartPage.java
@@ -0,0 +1,61 @@
+package com.kazmik.andro.icareprototype;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class StartPage extends ActionBarActivity {
+
+    Button blue,accel,speech;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+        blue = (Button)findViewById(R.id.bblue);
+        speech = (Button)findViewById(R.id.bspeech);
+        accel = (Button)findViewById(R.id.baccel);
+        accel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(StartPage.this,Accel.class));
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartPage.this, BlueChat.class));
+            }
+        });
+        speech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartPage.this, SpeechRecog.class));
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
